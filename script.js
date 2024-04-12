7a4513a67b15a4642ca4343445ee9baeab896d9c
Carlos Castillo <carlcastillo690@gmail.com>
2024-04-12 10:13:39 -0400

    completed part 9 of the JS breakout tutorial
    also added my own little bonus things to the game

--- script.js
+++ script.js
@@ -49,30 +49,33 @@ let colorString = document.getElementById("myColor");
 // brick vars
 /* Here we've defined the number of rows and columns of bricks, their width and height, the padding between the bricks 
 so they won't touch each other and a top and left offset so they won't start being drawn right from the edge of the Canvas. */
-const brickRowCount = 5;
-const brickColumnCount = 2;
+const brickRowCount = 3;
+const brickColumnCount = 5;
 const brickWidth = 75;
 const brickHeight = 20;
 const brickPadding = 10;
 const brickOffsetTop = 30;
 const brickOffsetLeft = 30;
+let score = 0; // game score counter for the game
 
 // list of bricks (Array)
 let bricks = [];
-
-// store bricks in array
+// initialize and store bricks in array
 /* We will hold all our bricks in a two-dimensional array. It will contain the brick columns (c), 
 which in turn will contain the brick rows (r), which in turn will each contain an object containing
-the x and y position to paint each brick on the screen */
+the x and y position to paint each brick on the screen and a status property that is 1 if it should be drawn
+or 0 if it shouldnt be drawn. */
 for (let c = 0; c < brickColumnCount; c++){
      bricks[c] = [];
      for (let r = 0; r < brickRowCount; r++){
-          bricks[c][r] = {x: 0, y: 0};
+          bricks[c][r] = {x: 0, y: 0, status: 1};
      }
 }
 // listen for key presses by setting up two event listenres
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+// listen for mouse movement
+document.addEventListener("mousemove",mouseMoveHandler,false);
 
 /*This function will detect for the event that user presses arrow right key (or right key for other browsers such as Edge),
 or arrow left key (or left key for other browsers such as Edge).*/
@@ -96,6 +99,71 @@ function keyUpHandler(e) {
      }
 }
 
+function mouseMoveHandler(e){
+     /*
+     In this function we first work out a relativeX value, which is equal 
+     to the horizontal mouse position in the viewport (e.clientX) minus the distance between the left edge of the canvas
+      and left edge of the viewport (canvas.offsetLeft) — effectively this is equal to the distance between the canvas left edge
+       and the mouse pointer. If the relative X pointer position is greater than zero and lower than the Canvas width, 
+       the pointer is within the Canvas boundaries, and the paddleX position (anchored on the left edge of the paddle) is 
+       set to the relativeX value minus half the width of the paddle, so that the movement will actually be relative to the 
+       middle of the paddle.
+
+     The paddle will now follow the position of the mouse cursor, but since we're restricting the movement to the size of the Canvas,
+     it won't disappear completely off either side.
+     */
+     const relativeX = e.clientX - canvas.offsetLeft;
+     if (relativeX > 0 && relativeX < canvas.width){
+          paddleX = relativeX - paddleWidth / 2;
+
+          // check if the paddle tries to leave the screen (when using mouse controls) from either wall
+          // then keep its x position within the left and right wall boundaries
+          if (paddleX < 0)
+          {
+               paddleX = 0;
+          }
+          else if (paddleX > canvas.width - paddleWidth){
+               paddleX = canvas.width - paddleWidth;
+          }
+     }
+}
+
+
+/* This function keeps track of when any bricks are hit by the ball.  
+It will make a brick disappear and reverse the ball if they make contact with each other. */
+function collisionDetection(){
+     for (let c = 0; c < brickColumnCount; c++){
+          for (let r = 0; r < brickRowCount; r++){
+               let b = bricks[c][r]; // declare a brick object
+               // status 1 means the brick has been drawn already so its  visible on the screen
+               if (b.status == 1){
+                    // check when the CENTER of the ball falls within a bricks coordinates then reverse balls direction
+                    // status is set to 0 so brick doesnt get drawn on next frame
+                    if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+                         dy = -dy;
+                         b.status = 0;
+                         score++; // increase player score when brick is hit
+                         if (score == brickRowCount * brickColumnCount){
+                              alert(`YOU WIN, CONGRATS! YOUR HIGH SCORE WAS ${score}`);
+                              document.location.reload();
+                              // clearInterval(interval);  for chrome users
+                         }
+                         // if you want to have the score go up by 2,3,4,etc.
+                         // try this approach
+                         /*
+                         score += 2; // increase player score when brick is hit
+                         if ((score / 2) == brickRowCount * brickColumnCount){
+                              alert(`YOU WIN, CONGRATS! YOUR HIGH SCORE WAS ${score}`);
+                              document.location.reload();
+                              // clearInterval(interval);  for chrome users
+                         }
+                          */
+                    }
+               }
+          }
+     }
+     
+}
 /*This function generates a random number in a range and decides upon the color from the list of colors based
 on the random number. */
 function getColorChoice(){
@@ -154,22 +222,33 @@ coordinates instead of (0,0) each time,
 function drawBricks(){
      for (let c = 0; c < brickColumnCount; c++){
           for (let r = 0; r < brickRowCount; r++){
-               let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-               let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-               bricks[c][r].x = brickX;
-               bricks[c][r].y = brickY;
-               ctx.beginPath();
-               ctx.rect(brickX,brickY, brickWidth, brickHeight);
-               ctx.fillStyle = "lime";
-               ctx.fill();
-               ctx.closePath();
+               if (bricks[c][r].status == 1){
+                    let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                    let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                    bricks[c][r].x = brickX;
+                    bricks[c][r].y = brickY;
+                    ctx.beginPath();
+                    ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                    ctx.fillStyle = "lime";
+                    ctx.fill();
+                    ctx.closePath();
+               }
+
           }
      }
+}
+
+/* This function will draw the score counter onto the canvas. */
+function drawScore(){
+     ctx.font = "16px Arial";
+     ctx.fillStyle = "white";
+     ctx.fillText(`Score:  ${score}`, 8, 20);
 }
 /*
 This function is the main drawing loop which will do the following 
 every 10 milliseconds :
-the canvas is cleared, bricks,ball, and paddle are drawn, and the x and y values will be updated 
+the canvas is cleared,draw all game objects, activate collision detection for bricks,
+and finally the x and y values of ball will be updated 
 for the next frame.
 */
 function draw(){
@@ -178,7 +257,9 @@ function draw(){
      drawBricks();
      drawBall(color);
      drawPaddle();
-     
+     drawScore();
+     collisionDetection();
+
      // display color of the ball as an html h2 (heading) element
      colorString.innerHTML = "current ball color :" + color;
 
@@ -206,9 +287,7 @@ function draw(){
           dy = -dy; // reverse the balls direction
           // change the color of the ball when it hits top  wall.
           color = getColorChoice();    
-     } else if (y + dy > canvas.height - ballRadius){
-          console.log(canvas.height)
-          
+     } else if (y + dy > canvas.height - ballRadius){          
           //When balls currrent position surpasses the bottom of the screen which is 
           // OLD (ignore this one) :(canvas height - ballRadius) = a position that is a little bit before the wall
           // NEW : canvas.height + paddleHeight = a position beyond the bottom wall (similar to how it works in the atari breakout)
@@ -224,7 +303,7 @@ function draw(){
                //then end the game and reload the webpage.
                alert("Game over!");
                document.location.reload();
-               clearInterval(interval);     
+               //clearInterval(interval);  this one is needed for chrome to end the game   
           }
           
 
@@ -234,17 +313,15 @@ function draw(){
      //Note: Try making the ball move faster when it hits the paddle.
 
 
-
-
      // when  paddle is moving right, move their x position by number of pixels
      // when the paddle goes beyond right wall then keep its x position as the canvas width - paddleWidth
      if (rightPressed && paddleX < canvas.width - paddleWidth){
-          paddleX += 10;
+          paddleX += 7;
      }
      else if (leftPressed && paddleX > 0){
           // when paddle is moving left, move their x position by number of pixels
           // when the paddle goes beyond left wall then keep its x position at 0
-          paddleX -= 10;
+          paddleX -= 7;
 
      }
 
